f511257e92411755d4636160699e5ccbdd6f76ac
webdeveloper510 <webdeveloper510@gmail.com>
2023-04-10 09:55:22 +0530

    chnages

--- src/component/pageNotfound/Page404.jsx
+++ src/component/pageNotfound/Page404.jsx
@@ -1,33 +1,21 @@
 import React from "react";
+import {links, NavLink, useNavigate} from 'react-router-dom';
+import notfound from '../../assets/img/userdashboard/error.webp';
 
 const Page404 = () =>{
     return(
         <>
-         <section className="why-us section-bgba verification_banner">
-    <div className="container">
-        <div className="row">
-            
-            <div className="col-lg-12">
-                {/* start-- card */}
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="card card-verification">
-                            <div className="card-body">
-                        
-                                <h5 className="Sign-heading">Page 404</h5>
-                                <div className="form_verification">
-      
-                                    <h1>Page Not Found 404 </h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* End-- card */}
+         <div className='container'>
+            <div className='notfound-section'>
+            <img src={notfound} alt="404error" />
+
+            <h1>OOPS !</h1>
+            <p>We can't seem to find the page you are looking for !</p>
+
+            <NavLink to="/">Go to Homepage</NavLink>
             </div>
-        </div>
-    </div>
-        </section>
+            </div>
+        
         </>
     )
 }
