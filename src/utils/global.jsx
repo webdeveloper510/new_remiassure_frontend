f7c23c25c476417d7c2c56ff2c6cd0a7f2a47e4f
webdeveloper510 <webdeveloper510@gmail.com>
2023-07-11 18:02:15 +0530

    new signup compo dev  server

--- src/utils/global.jsx
+++ src/utils/global.jsx
@@ -8,11 +8,11 @@ const global = {
   // serverUrl: "https://api.remitassure.com", // obi server Live
 
   //dev key
-  // stripe_p_key: 'pk_test_51JKK8iGym3ZLfIjemiCEFlNgTzompsdeASUafVtcC6ezwanyeb3Ckv4NggwRgAE0FOdWJbBdXrIAZxrhV1oM8aHr00432fKmYl'
+  stripe_p_key: 'pk_test_51JKK8iGym3ZLfIjemiCEFlNgTzompsdeASUafVtcC6ezwanyeb3Ckv4NggwRgAE0FOdWJbBdXrIAZxrhV1oM8aHr00432fKmYl'
 
 
   //live key
-  stripe_p_key: 'pk_live_51N0MChJLwqYh3SVNT0FRJAJnVbB7CHxheO4jwdvIh8TntdJYTt39i1cIAgORGzhSfUhqpDU2d9Z0fCj33tjNYY6A00TsB3IH5M'
+  // stripe_p_key: 'pk_live_51N0MChJLwqYh3SVNT0FRJAJnVbB7CHxheO4jwdvIh8TntdJYTt39i1cIAgORGzhSfUhqpDU2d9Z0fCj33tjNYY6A00TsB3IH5M'
 
 };
 
