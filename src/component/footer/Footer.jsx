d669e8bd377d9ad7565e1f878f8752b8662c70c3
nitin <diksha_sharma@codenomad.net>
2024-01-02 10:22:43 +0530

    done

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -405,7 +405,7 @@ const Footer = () => {
                                             <a className="twitter" target="_blank" href="https://twitter.com/remitassure">  <img src="assets/img/home/twiter.svg" /></a>
                                             <a className="facebook" target="_blank" href="https://www.facebook.com/remitassure"><img src="assets/img/home/facebook.svg" /></a>
                                             <a class="instagram" target="_blank" href="https://www.instagram.com/media.remitassure/"><img src="assets/img/footer/instagram.png" /></a>
-                                            <a className="instagram" target="_blank" href="https://www.linkedin.com/company/remitassure/"><img src="assets/img/footer/link1.png" /></a>
+                                            <a className="instagram" target="_blank" href="https://www.linkedin.com/company/remitassure/"><img src="assets/img/home/Linkedin Logo.svg" /></a>
                                         </div>
                                     </div>
                                 </div>
